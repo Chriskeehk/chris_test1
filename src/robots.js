9697c47800684912f1025ccb5bf8f2eace40c2af
unknown <chris@hkmouse.com>
2021-07-11 09:12:39 +0100

    First Version

---
+++ src/robots.js
@@ -0,0 +1,27 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Chris Ho',
+        username: 'Bret',
+        email: 'chris@hkmouse.com'
+    },
+    {
+        id: 2,
+        name: 'Yoie Cheung',
+        username: 'Yoie',
+        email: 'yoie@hkmouse.com'
+    },
+    {
+        id: 3,
+        name: 'Yanis Ho',
+        username: 'Yanis',
+        email: 'yanis@hkmouse.com'
+    },
+    {
+        id: 4,
+        name: 'Evellin Ho',
+        username: 'Evellin',
+        email: 'evellin@hkmouse.com'
+    }
+
+]

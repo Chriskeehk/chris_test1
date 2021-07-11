e524e587742809e25079e9eb69c13e05ad6cc4a7
unknown <chris@hkmouse.com>
2021-07-11 09:30:06 +0100

    Updates

---
+++ precache-manifest.02ecb2faeb56e14d751f78bfbebb5503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5f4c6815fd7da2eb880",
+    "url": "/Chriskeehk/chris_test1/static/css/main.7cb8c675.chunk.css"
+  },
+  {
+    "revision": "f5f4c6815fd7da2eb880",
+    "url": "/Chriskeehk/chris_test1/static/js/main.9cc28c77.chunk.js"
+  },
+  {
+    "revision": "348818685a1a2b76c7d2",
+    "url": "/Chriskeehk/chris_test1/static/js/runtime~main.6f04f99d.js"
+  },
+  {
+    "revision": "ae749c9ed96a6d2ec7fe",
+    "url": "/Chriskeehk/chris_test1/static/css/2.c907aa7a.chunk.css"
+  },
+  {
+    "revision": "ae749c9ed96a6d2ec7fe",
+    "url": "/Chriskeehk/chris_test1/static/js/2.a55fe2cd.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Chriskeehk/chris_test1/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fecc32f6d0d431b7ce1f327429a07bb9",
+    "url": "/Chriskeehk/chris_test1/index.html"
+  }
+];
